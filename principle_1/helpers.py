0c7b5380ec9dd796c65a207f996f56bded4e7be3
Michael James <mikeyj777@gmail.com>
2026-04-13 22:22:47 -0400

    markov example

--- principle_1/helpers.py
+++ principle_1/helpers.py
@@ -2,15 +2,17 @@ import numpy as np
 from scipy.linalg import expm
 
 class Chainlink:
-  def __init__(self, is_sunny, my_probability_of_existing, P = None, Q = None, parent = None):
+  def __init__(self, is_sunny, my_probability_of_existing = 1, P = None, Q = None, parent_id= None, day):
     self.P = P
     self.Q = Q
-    self.p = my_probability_of_existing
-    self.parent = parent
+    self.my_probability_of_existing = my_probability_of_existing
+    self.parent_id= parent_id
     self.children = []
-    self.is_sunny = int(is_sunny)
+    self.is_sunny = is_sunny
+    self.prob_row_num = (int(is_sunny) + 1) % 2
     self.probability_tmrw_is_sunny = None
-    self.me = id(self)
+    self.id = id(self)
+    self.day = day
     
   def get_P_at_t(self, t):
     return expm(t * self.Q)
@@ -22,6 +24,7 @@ class Chainlink:
     if P is None:
       P = self.P
     p_row = P[self.is_sunny]
-    self.probability_tmrw_is_sunny = self.p * p_row[0]
-    
+    self.probability_tmrw_is_sunny = self.my_probability_of_existing * p_row[0]
+  
+  
     
--- principle_1/p1_b5_markov_ex.py
+++ principle_1/p1_b5_markov_ex.py
@@ -22,14 +22,35 @@ from helpers import Chainlink
 
 P = np.array([[0.8, 0.2], [0.6, 0.4]])
 
+chain = []
 
-  
+def create_chain(is_sunny, parent_chainlink:Chainlink, target_day):
+  global chain
+  day = parent_chainlink.day
+  if day == target_day:
+    return
+  day += 1
+  parent_prob = parent_chainlink.my_probability_of_existing
+  parent_was_sunny = parent_chainlink.is_sunny
+  prob_row = P[(int(parent_was_sunny) + 1) % 2]
+  prob = prob_row[(int(is_sunny) + 1) % 2]
+  link = Chainlink(is_sunny=is_sunny, my_probability_of_existing=prob, P=P, parent_id=parent_chainlink.id)
+  chain.append(link)
+  create_chain
+
 
 # If today is Sunny, what's the probability distribution after 1 day? After 5 days? After 20 days?
 num_days = [1, 5, 20]
 
 init_row = 0
 results = {}
+is_sunny = True
+prob_of_existing = 1
 for days in num_days:
-  for day in range(1, days + 1):
+  parent_link = Chainlink(is_sunny=is_sunny, my_probability_of_existing=prob_of_existing, P = P)
+  parent_id = link0.me
+  for day in days:
+    for is_sunny in [True, False]:
+      link = Chainlink(is_sunny=is_sunny)
+
     
