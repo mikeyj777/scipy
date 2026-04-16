55cb2c47e77b68e0e69a09c26338ff45df3596cc
Michael James <mikeyj777@gmail.com>
2026-04-16 06:46:58 -0400

    markov ex

--- principle_1/helpers.py
+++ principle_1/helpers.py
@@ -1,30 +1,41 @@
 import numpy as np
 from scipy.linalg import expm
 
+link_dict = {}
+
 class Chainlink:
-  def __init__(self, is_sunny, my_probability_of_existing = 1, P = None, Q = None, parent_id= None, day):
+  def __init__(self, is_sunny, target_days, my_probability_of_existing = 1, current_day = 0, P = None, Q = None, parent_id= None):
+    global link_dict
     self.P = P
     self.Q = Q
-    self.my_probability_of_existing = my_probability_of_existing
-    self.parent_id= parent_id
-    self.children = []
+    self.my_probability_of_existing = float(my_probability_of_existing)
     self.is_sunny = is_sunny
-    self.prob_row_num = (int(is_sunny) + 1) % 2
-    self.probability_tmrw_is_sunny = None
+    self.target_days = target_days
+    self.current_day = current_day
+    self.parent_id= parent_id
+    prob_row_idx = (int(is_sunny) + 1) % 2
+    prob_row = self.P[prob_row_idx]
+    self.prob_tmrw_is_sunny = prob_row[0]
     self.id = id(self)
-    self.day = day
+    self.children = []
+    if current_day not in link_dict:
+      link_dict[current_day] = []
+    link_dict[current_day].append(self)
+    if current_day < target_days:
+      self.make_children()
+      
     
   def get_P_at_t(self, t):
     return expm(t * self.Q)
   
+  def get_link_dict(self):
+    return link_dict
+  
   def set_P(self, P):
     self.P = P
-  
-  def set_probabilities(self, P = None):
-    if P is None:
-      P = self.P
-    p_row = P[self.is_sunny]
-    self.probability_tmrw_is_sunny = self.my_probability_of_existing * p_row[0]
-  
-  
+    
+  def make_children(self):
+    #tomorrow is sunny
+    self.children.append(Chainlink(is_sunny=True, target_days=self.target_days, my_probability_of_existing=self.my_probability_of_existing * self.prob_tmrw_is_sunny, current_day=self.current_day + 1, P = self.P, Q=self.Q, parent_id=self.id))
+    self.children.append(Chainlink(is_sunny=False, target_days=self.target_days, my_probability_of_existing=self.my_probability_of_existing * (1-self.prob_tmrw_is_sunny), current_day=self.current_day + 1, P = self.P, Q=self.Q, parent_id=self.id))
     
--- principle_1/p1_b5_markov_ex.py
+++ principle_1/p1_b5_markov_ex.py
@@ -26,35 +26,17 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from principle_1.helpers import Chainlink
 
 P = np.array([[0.8, 0.2], [0.6, 0.4]])
-starting_weather_is_sunny = True
-chain = []
 
-def create_chain(is_sunny, parent_chainlink:Chainlink, target_day):
-  global chain
-  day = parent_chainlink.day
-  if day == target_day:
-    return
-  shared_chainlink = parent_chainlink
-  parent_prob = parent_chainlink.my_probability_of_existing
-  parent_was_sunny = parent_chainlink.is_sunny
-  prob_row = P[(int(parent_was_sunny) + 1) % 2]
-  prob = prob_row[(int(is_sunny) + 1) % 2]
-  link = Chainlink(is_sunny=is_sunny, my_probability_of_existing=prob, P=P, parent_id=parent_chainlink.id)
-  chain.append(link)
-  create_chain(is_sunny=is_sunny, parent_chainlink=shared_chainlink, target_day=target_day)
 
 
 # If today is Sunny, what's the probability distribution after 1 day? After 5 days? After 20 days?
 num_days = [1, 5, 20]
-
-results = {}
-is_sunny = starting_weather_is_sunny
-prob_of_existing = 1
+starting_weather_is_sunny = True
 for days in num_days:
-  parent_link = Chainlink(is_sunny=is_sunny, my_probability_of_existing=prob_of_existing, P = P)
-  parent_id = link0.me
-  for day in days:
-    for is_sunny in [True, False]:
-      link = Chainlink(is_sunny=is_sunny)
-
-    
+  link = Chainlink(is_sunny=starting_weather_is_sunny, target_days=days, P=P)
+  link_dict = link.get_link_dict()
+  links_after_x_days = link_dict[days]
+  for link_future in links_after_x_days:
+    if link_future.is_sunny:
+      # sum probs for each day, sunny vs not
+      # print(f'day {days} | prob is sunny: {link_future.my_probability_of_existing}')
